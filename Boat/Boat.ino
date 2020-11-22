0436af21d79507b27711d2643372b8b61c0b4096
qmaster0803 <qmaster080305@gmail.com>
2020-11-23 00:34:01 +0300

    full rc added, telemetry wip

--- Boat/Boat.ino
+++ Boat/Boat.ino
@@ -1,15 +1,47 @@
 #include "ESC.h"
 #include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+#include "power.h"
 
-Servo rudder;
-ESC right (5, 1000, 2000, 500);
-ESC left (4, 1000, 2000, 500);
-ESC front (2, 1000, 2000, 500);
-ESC back (3, 1000, 2000, 500);
+#define HEARTRATE_PERIOD 500
+#define TELEMETRY_RATE   300
+
+#define RUDDER_COMMAND           0x00
+#define SIDE_COMMAND             0x01
+#define FORWARD_COMMAND          0x02
+#define RUDDER_POSITION_COMMAND  0x03
+#define RUDDER_ENDPOINTS_COMMAND 0x04
+
+ESC rudder (A0, 1000, 2000, 1500);
+ESC right  (5, 1000, 2000, 500);
+ESC left   (4, 1000, 2000, 500);
+ESC front  (2, 1000, 2000, 500);
+ESC back   (3, 1000, 2000, 500);
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+PowerModule pm(A15, A14);
+
+byte radio_buffer[10];
+byte radio_bytes_count = 0;
+unsigned long radio_command_time = 0;
+unsigned int engine_left_value = 1000;
+unsigned int engine_right_value = 1000;
+unsigned int engine_front_value = 1000;
+unsigned int engine_back_value = 1000;
+unsigned int rudder_value = 1500;
+unsigned long last_signal_received = 0;
+unsigned long last_telemetry_send = 0;
+
+byte rudder_position = 127;
+byte rudder_endpoints = 127;
+
+bool signal_active = false;
 
 void setup() {
+  Serial.begin(9600);
   Serial1.begin(57600);
-  rudder.attach(A0);
+  rudder.arm();
   right.arm();
   left.arm();
   front.arm();
@@ -17,48 +49,85 @@ void setup() {
 }
 
 void loop() {
-  if (Serial1.available() > 1) {
-    int command = Serial1.read();
-    int value = Serial1.read();
-    if(command == 'R') //rudder
-    {
-      rudder.write(value);
-    }
-    if(command == 'S') //side
-    {
-      if(value > 127)
-      {
-        left.speed(map(value, 127, 254, 1000, 1300));
-        right.speed(1000);
-      }
-      if(value < 127)
-      {
-        right.speed(map(value, 127, 0, 1000, 1300));
-        left.speed(1000);
-      }
-      if(value == 127)
-      {
-        right.speed(1000);
-        left.speed(1000);
-      }
-    }
-    if(command == 'F') //forward
-    {
-      if(value > 127)
-      {
-        front.speed(map(value, 127, 254, 1000, 1300));
-        back.speed(1000);
-      }
-      if(value < 127)
-      {
-        back.speed(map(value, 127, 0, 1000, 1300));
-        front.speed(1000);
-      }
-      if(value == 127)
-      {
-        front.speed(1000);
-        back.speed(1000);
-      }
-    }
+  if (Serial1.available() > 0)
+  {
+    radio_buffer[radio_bytes_count] = Serial1.read();
+    radio_bytes_count++;
+    radio_command_time = millis();
+    signal_active = true;
+  }
+  if(radio_bytes_count > 0 && millis()-radio_command_time > HEARTRATE_PERIOD)
+  {
+    radio_bytes_count = 0;
+  }
+  if(radio_buffer[radio_bytes_count-1] == 0xFF && radio_bytes_count >= 3)
+  {
+    handle_incoming_command();
+    update_engines();
+    radio_bytes_count = 0;
+  }
+  if(millis() - radio_command_time > HEARTRATE_PERIOD && signal_active)
+  {
+    do_failsafe();
+    signal_active = false;
+  }
+  if(millis() - last_telemetry_send > TELEMETRY_RATE)
+  {
+    send_telemetry();
+  }
+}
+
+void send_telemetry()
+{
+  
+}
+
+void send_float(float value)
+{
+  send_command(
+}
+
+void do_failsafe()
+{
+  radio_bytes_count = 0;
+  engine_left_value = 1000;
+  engine_right_value = 1000;
+  engine_front_value = 1000;
+  engine_back_value = 1000;
+  rudder_value = 1500;
+  update_engines();
+}
+
+void update_engines()
+{
+  left.speed(engine_left_value);
+  right.speed(engine_right_value);
+  front.speed(engine_front_value);
+  back.speed(engine_back_value);
+  rudder.speed(rudder_value+map(rudder_position, 0, 254, -200, 200));
+}
+
+void handle_incoming_command()
+{
+  if(radio_buffer[0] == RUDDER_COMMAND) {rudder_value = map(radio_buffer[1], 0, 254, 1250, 1750);}
+  if(radio_buffer[0] == SIDE_COMMAND)
+  {
+    if(radio_buffer[1] > 127) {engine_left_value = map(radio_buffer[1], 127, 254, 1000, 1300); engine_right_value = 1000;}
+    if(radio_buffer[1] < 127) {engine_left_value = 1000; engine_right_value = map(radio_buffer[1], 127, 0, 1000, 1300);}
+    if(radio_buffer[1] == 127) {engine_left_value = 1000; engine_right_value = 1000;}
+  }
+  if(radio_buffer[0] == FORWARD_COMMAND)
+  {
+    if(radio_buffer[1] > 127) {engine_front_value = map(radio_buffer[1], 127, 254, 1090, 1300); engine_back_value = 1000;}
+    if(radio_buffer[1] < 127) {engine_front_value = 1000; engine_back_value = map(radio_buffer[1], 127, 0, 1090, 1300);}
+    if(radio_buffer[1] == 127) {engine_front_value = 1000; engine_back_value = 1000;}
+  }
+  if(radio_buffer[0] == RUDDER_POSITION_COMMAND)
+  {
+    rudder_position = radio_buffer[1];
+  }
+  if(radio_buffer[0] == RUDDER_ENDPOINTS_COMMAND)
+  {
+    rudder_endpoints = radio_buffer[1];
   }
 }
--- Remote/Remote.ino
+++ Remote/Remote.ino
@@ -1,53 +1,207 @@
+#include <PCD8544.h>
+#include <Debounce.h>
+#include <EEPROM.h>
+
+#define RUDDER_COMMAND           0x00
+#define SIDE_COMMAND             0x01
+#define FORWARD_COMMAND          0x02
+#define RUDDER_POSITION_COMMAND  0x03
+#define RUDDER_ENDPOINTS_COMMAND 0x04
+
+#define RUDDER_POSITION_ADDR   0x00
+#define RUDDER_ENDPOINTS_ADDR  0x02
+
+#define DEFAULT_RUDDER_POSITION   0
+#define DEFAULT_RUDDER_ENDPOINTS  45
+
+#define MENU_COUNT        2
+#define HEARTRATE_PERIOD  300
+
 int prev_rudder = -1;
 int prev_side = -1;
 int prev_forward = -1;
+unsigned long prev_left_button = 0;
+unsigned long prev_center_button = 0;
+unsigned long prev_right_button = 0;
 
-void setup() {
+byte active_menu = 0;
+byte menu_pos = 0;
+bool is_editing = false;
+int value_in_edit = 0;
+bool value_read = false;
+bool update_data_p_display = true;
+
+unsigned long last_radio_sent = 0;
+
+const char menu1[] PROGMEM = "Rudder pos.";
+const char menu2[] PROGMEM = "Rudder endp.";
+
+const char* const menu[] PROGMEM = {
+  menu1, //rudder position
+  menu2  //rudder endpoints
+};
+const byte eeprom_addr[] PROGMEM = {
+  RUDDER_POSITION_ADDR,
+  RUDDER_ENDPOINTS_ADDR
+};
+
+const byte settings_commands[] = {
+  RUDDER_POSITION_COMMAND,
+  RUDDER_ENDPOINTS_COMMAND
+};
+
+const byte active_glyph[] = {0x00, 0x7e, 0x3c, 0x18, 0x00};
+
+static PCD8544 p_display;
+Debounce button_left (A3, 1);
+Debounce button_center (A4, 1);
+Debounce button_right (A5, 1);
+
+void setup()
+{
+  pinMode(A3, INPUT_PULLUP);
+  pinMode(A4, INPUT_PULLUP);
+  pinMode(A5, INPUT_PULLUP);
   Serial.begin(9600);
   Serial1.begin(57600);
+  p_display.begin(84, 48);
+  p_display.createChar(0, active_glyph);
 }
 
-void loop() {
+void loop()
+{
+  update_rc_values();
+  handle_buttons();
+  update_p_display();
+  if(millis() - last_radio_sent > HEARTRATE_PERIOD)
+  {
+    send_radio_command(0xFF, 0xFF);
+  }
+  delay(50);
+}
+
+void handle_buttons()
+{
+  if(prev_right_button < button_right.count())
+  {
+    if(is_editing && value_in_edit < 127) {value_in_edit++;}
+    if(!is_editing && active_menu < MENU_COUNT-1) {active_menu++;}
+    prev_right_button = button_right.count();
+    update_data_p_display = true;
+  }
+  if(prev_left_button < button_left.count())
+  {
+    if(is_editing && value_in_edit > -127) {value_in_edit--;}
+    if(active_menu > 0) {active_menu--;}
+    prev_left_button = button_left.count();
+    update_data_p_display = true;
+  }
+  if(prev_center_button < button_center.count())
+  {
+    is_editing = !is_editing;
+    prev_center_button = button_center.count();
+    update_data_p_display = true;
+  }
+}
+
+void update_p_display()
+{
+  if(update_data_p_display)
+  {
+    if(!is_editing)
+    {
+      if(value_read)
+      {
+        eeprom_write_word(eeprom_addr[active_menu], value_in_edit);
+        send_radio_command(settings_commands[active_menu], 127+value_in_edit);
+        value_read = false;
+      }
+      p_display.clear();
+      byte to_show = MENU_COUNT-menu_pos;
+      byte first_index = menu_pos;
+      byte second_index = menu_pos+to_show;
+      byte counter = 0;
+      char progmemBuf[16];
+      for(int i = first_index; i < second_index; i++)
+      {
+        strcpy_P(progmemBuf, pgm_read_word(&(menu[i])));
+        p_display.setCursor(0, 1*counter);
+        if(i == active_menu) {p_display.write(0);}
+        else {p_display.write(20);}
+        p_display.print(progmemBuf);
+        counter++;
+      }
+    }
+    else
+    {
+      if(!value_read)
+      {
+        p_display.clear();
+        value_in_edit = eeprom_read_word(eeprom_addr[active_menu]);
+        value_read = true;
+        char progmemBuf[16];
+        strcpy_P(progmemBuf, pgm_read_word(&(menu[active_menu])));
+        p_display.setCursor(0, 0);
+        p_display.print(progmemBuf);
+        p_display.setCursor(0, 2);
+        p_display.write(0);
+        p_display.print(value_in_edit);
+      }
+      else
+      {
+        p_display.setCursor(0, 2);
+        p_display.clearLine();
+        p_display.write(0);
+        p_display.print(value_in_edit);
+      }
+    }
+    update_data_p_display = false;
+  }
+}
+
+void update_rc_values()
+{
   int now_side = analogRead(A0);
   int now_forward = analogRead(A1);
   int now_rudder = analogRead(A2);
-  if((now_rudder > 520 || now_rudder < 510) && now_rudder != prev_rudder) //rudder
+  if((now_rudder > 520 || now_rudder < 515) && now_rudder != prev_rudder)
   {
-    Serial1.write('R'); Serial1.write(map(analogRead(A2), 0, 1024, 45, 135));
+    send_radio_command(RUDDER_COMMAND, map(now_rudder, 1024, 0, 0, 254));
     prev_rudder = now_rudder;
-    Serial.println("Rudder active");
   }
-  else if(now_rudder < 520 && now_rudder > 510 && now_rudder != prev_rudder)
+  else if(now_rudder < 520 && now_rudder > 515 && now_rudder != prev_rudder)
   {
-    Serial1.write('R'); Serial1.write(90);
+    send_radio_command(RUDDER_COMMAND, 127);
     prev_rudder = now_rudder;
-    Serial.println("Rudder centered");
   }
 
-  if((now_side > 605 || now_side < 510) && now_side != prev_side) //side
+  if((now_side > 530 || now_side < 430) && now_side != prev_side)
   {
-    Serial1.write('S'); Serial1.write(map(now_side, 0, 1023, 0, 254));
+    send_radio_command(SIDE_COMMAND, map(now_side, 1023, 0, 0, 254));
     prev_side = now_side;
-    Serial.println("Side active");
   }
-  else if(now_side < 605 && now_side > 510 && now_side != prev_side)
+  else if(now_side < 530 && now_side > 430 && now_side != prev_side)
   {
-    Serial1.write('S'); Serial1.write(127);
+    send_radio_command(SIDE_COMMAND, 127);
     prev_side = now_side;
-    Serial.println("Side stopped");
   }
 
-  if((now_forward > 510 || now_forward < 495) && now_forward != prev_forward) //forward
+  if((now_forward > 505 || now_forward < 500) && now_forward != prev_forward)
   {
-    Serial1.write('F'); Serial1.write(map(now_forward, 0, 1023, 0, 254));
+    send_radio_command(FORWARD_COMMAND, map(now_forward, 1024, 0, 0, 254));
     prev_forward = now_forward;
-    Serial.println("Forward active");
   }
-  else if(now_forward < 510 && now_forward > 495 && now_forward != prev_forward)
+  else if(now_forward < 505 && now_forward > 500 && now_forward != prev_forward)
   {
-    Serial1.write('F'); Serial1.write(127);
+    send_radio_command(FORWARD_COMMAND, 127);
     prev_forward = now_forward;
-    Serial.println("Forward stopped");
   }
-  delay(50);
+}
+
+void send_radio_command(byte command, byte value)
+{
+  Serial1.write(command);
+  Serial1.write(value);
+  Serial1.write(0xFF);
+  last_radio_sent = millis();
 }
