16f6515a23437a3e942bf49d9042d42ff888f8c4
qmaster0803 <qmaster080305@gmail.com>
2020-11-30 00:23:34 +0300

    telemetry updated, wip

--- Boat/Boat.ino
+++ Boat/Boat.ino
@@ -3,16 +3,20 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
+#include <iarduino_GPS_NMEA.h>
 #include "power.h"
 
 #define HEARTRATE_PERIOD 500
-#define TELEMETRY_RATE   300
 
 #define RUDDER_COMMAND           0x00
 #define SIDE_COMMAND             0x01
 #define FORWARD_COMMAND          0x02
 #define RUDDER_POSITION_COMMAND  0x03
 #define RUDDER_ENDPOINTS_COMMAND 0x04
+
+#define COMMAND_LATITUDE       0x10
+#define COMMAND_LONGITUDE      0x11
+#define TELEMETRY_SEND_COMMAND 0x12
 
 ESC rudder (A0, 1000, 2000, 1500);
 ESC right  (5, 1000, 2000, 500);
@@ -21,6 +25,7 @@ ESC front  (2, 1000, 2000, 500);
 ESC back   (3, 1000, 2000, 500);
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 PowerModule pm(A15, A14);
+iarduino_GPS_NMEA gps;
 
 byte radio_buffer[10];
 byte radio_bytes_count = 0;
@@ -31,21 +36,31 @@ unsigned int engine_front_value = 1000;
 unsigned int engine_back_value = 1000;
 unsigned int rudder_value = 1500;
 unsigned long last_signal_received = 0;
-unsigned long last_telemetry_send = 0;
 
 byte rudder_position = 127;
 byte rudder_endpoints = 127;
 
 bool signal_active = false;
+bool telemetry_send = false;
+
+union send_float_union
+{
+  float union_float;
+  int union_int;
+  byte union_bytes[4];
+};
 
 void setup() {
   Serial.begin(9600);
   Serial1.begin(57600);
+  Serial2.begin(9600);
   rudder.arm();
   right.arm();
   left.arm();
   front.arm();
   back.arm();
+  gps.begin(Serial2);
+  gps.timeZone(GPS_AutoDetectZone);
 }
 
 void loop() {
@@ -71,20 +86,42 @@ void loop() {
     do_failsafe();
     signal_active = false;
   }
-  if(millis() - last_telemetry_send > TELEMETRY_RATE)
+  if(telemetry_send)
   {
     send_telemetry();
+    telemetry_send = false;
   }
 }
 
 void send_telemetry()
 {
-  
+  //gps.read(i);
+  send_float(1.12);   //longitude
+  send_float(2.101);  //latitude
+  send_int(55);
+  send_int(999);
 }
 
 void send_float(float value)
 {
-  send_command(
+  union send_float_union un;
+  un.union_float = value;
+  send_bytes(un.union_bytes, 4);
+}
+
+void send_int(int value)
+{
+  union send_float_union un;
+  un.union_int = value;
+  send_bytes(un.union_bytes, 2);
+}
+
+void send_bytes(byte values[], byte counter)
+{
+  for(int i = 0; i < counter; i++)
+  {
+    Serial1.write(values[i]);
+  }
 }
 
 void do_failsafe()
@@ -122,12 +159,7 @@ void handle_incoming_command()
     if(radio_buffer[1] < 127) {engine_front_value = 1000; engine_back_value = map(radio_buffer[1], 127, 0, 1090, 1300);}
     if(radio_buffer[1] == 127) {engine_front_value = 1000; engine_back_value = 1000;}
   }
-  if(radio_buffer[0] == RUDDER_POSITION_COMMAND)
-  {
-    rudder_position = radio_buffer[1];
-  }
-  if(radio_buffer[0] == RUDDER_ENDPOINTS_COMMAND)
-  {
-    rudder_endpoints = radio_buffer[1];
-  }
+  if(radio_buffer[0] == RUDDER_POSITION_COMMAND) {rudder_position = radio_buffer[1];}
+  if(radio_buffer[0] == RUDDER_ENDPOINTS_COMMAND) {rudder_endpoints = radio_buffer[1];}
+  if(radio_buffer[0] == TELEMETRY_SEND_COMMAND) {telemetry_send = true;}
 }
--- Remote/Remote.ino
+++ Remote/Remote.ino
@@ -1,6 +1,8 @@
 #include <PCD8544.h>
 #include <Debounce.h>
 #include <EEPROM.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 #define RUDDER_COMMAND           0x00
 #define SIDE_COMMAND             0x01
@@ -8,14 +10,17 @@
 #define RUDDER_POSITION_COMMAND  0x03
 #define RUDDER_ENDPOINTS_COMMAND 0x04
 
+#define COMMAND_LATITUDE   0x10
+#define COMMAND_LONGITUDE  0x11
+#define TELEMETRY_SEND     0x12
+
 #define RUDDER_POSITION_ADDR   0x00
 #define RUDDER_ENDPOINTS_ADDR  0x02
 
-#define DEFAULT_RUDDER_POSITION   0
-#define DEFAULT_RUDDER_ENDPOINTS  45
-
-#define MENU_COUNT        2
-#define HEARTRATE_PERIOD  300
+#define MENU_COUNT            2
+#define HEARTRATE_PERIOD      300
+#define TELEMETRY_RATE        500
+#define MAX_TELEMETRY_PING    300
 
 int prev_rudder = -1;
 int prev_side = -1;
@@ -32,6 +37,7 @@ bool value_read = false;
 bool update_data_p_display = true;
 
 unsigned long last_radio_sent = 0;
+unsigned long last_telemetry_update = 0;
 
 const char menu1[] PROGMEM = "Rudder pos.";
 const char menu2[] PROGMEM = "Rudder endp.";
@@ -52,10 +58,36 @@ const byte settings_commands[] = {
 
 const byte active_glyph[] = {0x00, 0x7e, 0x3c, 0x18, 0x00};
 
-static PCD8544 p_display;
+static PCD8544 p_display(4, 5, 6, 7, 8);
+LiquidCrystal_I2C main_lcd(0x27,16,2);
+LiquidCrystal_I2C gps_lcd(0x26,16,2);
 Debounce button_left (A3, 1);
 Debounce button_center (A4, 1);
 Debounce button_right (A5, 1);
+
+union telemetry_union
+{
+  float union_float;
+  int union_int;
+  byte union_bytes[4];
+};
+
+byte satellite_char1[] = {0x06,0x06,0x06,0x1F,0x1F,0x06,0x06,0x06};
+byte satellite_char2[] = {0x02,0x09,0x05,0x15,0x15,0x05,0x09,0x02};
+byte accuracy_char[] = {0x00,0x04,0x04,0x0E,0x1B,0x0E,0x04,0x04};
+
+float longitude = 0;
+float latitude = 0;
+int DOP = 0;
+int satellites = 0;
+float last_longitude = -1;
+float last_latitude = -1;
+int last_DOP = -1;
+int last_satellites = -1;
+
+bool connected = false;
+bool initialized = false;
+bool error_connection_showed = false; //for p_display update
 
 void setup()
 {
@@ -66,6 +98,13 @@ void setup()
   Serial1.begin(57600);
   p_display.begin(84, 48);
   p_display.createChar(0, active_glyph);
+  main_lcd.init();
+  main_lcd.backlight();
+  gps_lcd.init();
+  gps_lcd.createChar(0, satellite_char1);
+  gps_lcd.createChar(1, satellite_char2);
+  gps_lcd.createChar(2, accuracy_char);
+  gps_lcd.backlight();
 }
 
 void loop()
@@ -73,11 +112,87 @@ void loop()
   update_rc_values();
   handle_buttons();
   update_p_display();
+  if(millis() - last_telemetry_update > TELEMETRY_RATE)
+  {
+    read_telemetry();
+    update_gps_lcd();
+    last_telemetry_update = millis();
+  }
   if(millis() - last_radio_sent > HEARTRATE_PERIOD)
   {
     send_radio_command(0xFF, 0xFF);
   }
-  delay(50);
+  delay(20);
+}
+
+void read_telemetry()
+{
+  send_radio_command(TELEMETRY_SEND, 0);
+  bool got = false;
+  unsigned long start_time = millis();
+  while(Serial1.available() > 0) {Serial1.read();}
+  while(!got && millis() - start_time < MAX_TELEMETRY_PING)
+  {
+    if(Serial1.available() == 12) {got = true;}
+  }
+  byte buffer[12];
+  for(int i = 0; i < 12; i++) {buffer[i] = Serial1.read();}
+  if(got)
+  {
+    connected = true;
+    initialized = true;
+    union telemetry_union un;
+    un.union_bytes[0] = buffer[0]; un.union_bytes[1] = buffer[1]; un.union_bytes[2] = buffer[2]; un.union_bytes[3] = buffer[3];
+    longitude = un.union_float;
+    un.union_bytes[0] = buffer[4]; un.union_bytes[1] = buffer[5]; un.union_bytes[2] = buffer[6]; un.union_bytes[3] = buffer[7];
+    latitude = un.union_float;
+    un.union_bytes[0] = buffer[8]; un.union_bytes[1] = buffer[9];
+    satellites = un.union_int;
+    un.union_bytes[0] = buffer[10]; un.union_bytes[1] = buffer[11];
+    DOP = un.union_int;
+  }
+  else
+  {
+    connected = false;
+  }
+}
+
+void update_gps_lcd()
+{
+  if(longitude != last_longitude || latitude != last_latitude || satellites != last_satellites || DOP != last_DOP)
+  {
+    gps_lcd.clear();
+    
+    gps_lcd.setCursor(0, 0);
+    if(longitude > 0) gps_lcd.print("N");
+    else if(longitude < 0) gps_lcd.print("S");
+    else gps_lcd.print(0);
+    gps_lcd.print("000");
+    gps_lcd.setCursor(3, 0);
+    gps_lcd.print(latitude, 5);
+    
+    gps_lcd.setCursor(0, 1);
+    if(longitude > 0) gps_lcd.print("E");
+    else if(longitude < 0) gps_lcd.print("W");
+    else gps_lcd.print(0);
+    gps_lcd.print("000");
+    gps_lcd.setCursor(3, 1);
+    gps_lcd.print(longitude, 5);
+    
+    gps_lcd.setCursor(12, 0);
+    gps_lcd.write(0);
+    gps_lcd.write(1);
+    gps_lcd.print(satellites);
+
+    gps_lcd.setCursor(12, 1);
+    gps_lcd.write(2);
+    gps_lcd.print(DOP);
+    
+    last_longitude = longitude;
+    last_latitude = latitude;
+    last_satellites = satellites;
+    last_DOP = DOP;
+  }
 }
 
 void handle_buttons()
@@ -106,56 +221,82 @@ void handle_buttons()
 
 void update_p_display()
 {
-  if(update_data_p_display)
+  if(initialized && connected)
   {
-    if(!is_editing)
+    if(update_data_p_display || error_connection_showed)
     {
-      if(value_read)
+      if(!is_editing)
       {
-        eeprom_write_word(eeprom_addr[active_menu], value_in_edit);
-        send_radio_command(settings_commands[active_menu], 127+value_in_edit);
-        value_read = false;
-      }
-      p_display.clear();
-      byte to_show = MENU_COUNT-menu_pos;
-      byte first_index = menu_pos;
-      byte second_index = menu_pos+to_show;
-      byte counter = 0;
-      char progmemBuf[16];
-      for(int i = first_index; i < second_index; i++)
-      {
-        strcpy_P(progmemBuf, pgm_read_word(&(menu[i])));
-        p_display.setCursor(0, 1*counter);
-        if(i == active_menu) {p_display.write(0);}
-        else {p_display.write(20);}
-        p_display.print(progmemBuf);
-        counter++;
-      }
-    }
-    else
-    {
-      if(!value_read)
-      {
+        if(value_read)
+        {
+          eeprom_write_word(eeprom_addr[active_menu], value_in_edit);
+          send_radio_command(settings_commands[active_menu], 127+value_in_edit);
+          value_read = false;
+        }
         p_display.clear();
-        value_in_edit = eeprom_read_word(eeprom_addr[active_menu]);
-        value_read = true;
+        byte to_show = MENU_COUNT-menu_pos;
+        byte first_index = menu_pos;
+        byte second_index = menu_pos+to_show;
+        byte counter = 0;
         char progmemBuf[16];
-        strcpy_P(progmemBuf, pgm_read_word(&(menu[active_menu])));
-        p_display.setCursor(0, 0);
-        p_display.print(progmemBuf);
-        p_display.setCursor(0, 2);
-        p_display.write(0);
-        p_display.print(value_in_edit);
+        for(int i = first_index; i < second_index; i++)
+        {
+          strcpy_P(progmemBuf, pgm_read_word(&(menu[i])));
+          p_display.setCursor(0, 1*counter);
+          if(i == active_menu) {p_display.write(0);}
+          else {p_display.write(20);}
+          p_display.print(progmemBuf);
+          counter++;
+        }
       }
       else
       {
-        p_display.setCursor(0, 2);
-        p_display.clearLine();
-        p_display.write(0);
-        p_display.print(value_in_edit);
+        if(!value_read)
+        {
+          p_display.clear();
+          value_in_edit = eeprom_read_word(eeprom_addr[active_menu]);
+          value_read = true;
+          char progmemBuf[16];
+          strcpy_P(progmemBuf, pgm_read_word(&(menu[active_menu])));
+          p_display.setCursor(0, 0);
+          p_display.print(progmemBuf);
+          p_display.setCursor(0, 2);
+          p_display.write(0);
+          p_display.print(value_in_edit);
+        }
+        else
+        {
+          p_display.setCursor(0, 2);
+          p_display.clearLine();
+          p_display.write(0);
+          p_display.print(value_in_edit);
+        }
       }
+      update_data_p_display = false;
     }
-    update_data_p_display = false;
+    error_connection_showed = false;
+  }
+  else if(initialized && !connected)
+  {
+    if(!error_connection_showed)
+    {
+      p_display.clear();
+      p_display.print("Connection");
+      p_display.setCursor(0, 1);
+      p_display.print("break!");
+      error_connection_showed = true;
+    }
+  }
+  else
+  {
+    if(!error_connection_showed)
+    {
+      p_display.clear();
+      p_display.print("Waiting for");
+      p_display.setCursor(0, 1);
+      p_display.print("connection...");
+      error_connection_showed = true;
+    }
   }
 }
 
